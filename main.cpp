d949244b49573b8689a7453c4ca05e66c30b05b9
dgo721 <dgo2389@hotmail.com>
2014-04-20 20:55:39 -0500

    alpha-0.1.6

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
-using namespace std;
 #include <mmsystem.h>
 #define GLUT_DISABLE_ATEXIT_HACK
 #ifdef __APPLE__
@@ -16,6 +15,10 @@ using namespace std;
 #else
 #include <GL/glut.h>
 #endif
+
+#include "imageloader.h"
+
+using namespace std;
 
 const int X_MAX=1;
 const int X_MIN=-1;
@@ -59,6 +62,33 @@ GLfloat mat_diffuse[][5] = {{0.0f, 0.0f, 0.0f, 1.0f},
                             {0.0f, 0.65f, 0.15f, 1.0f},
                             {0.32f, 0.35f, 0.12f, 1.0f}};
 GLfloat mat_specular[] = {1.0,1.0,1.0,1.0};
+
+GLuint loadTexture(Image* image) {
+	GLuint textureId;
+	glGenTextures(1, &textureId);
+	glBindTexture(GL_TEXTURE_2D, textureId);
+
+	glTexImage2D(GL_TEXTURE_2D,
+				 0,
+				 GL_RGB,
+				 image->width, image->height,
+				 0,
+				 GL_RGB,
+				 GL_UNSIGNED_BYTE,
+				 image->pixels);
+	return textureId;
+}
+
+GLuint _textureId;
+
+void initRendering() {
+	//glEnable(GL_NORMALIZE);
+	//glEnable(GL_COLOR_MATERIAL);
+
+	Image* image = loadBMP("/Users/Diego/Documents/CodeBlocks/ProjectLYNX/texturas/vtr.bmp");
+	_textureId = loadTexture(image);
+	delete image;
+}
 
 static void init()
 {
@@ -118,6 +148,30 @@ void coloreaCubo(int x){
     }
 }
 
+void drawQuad(){
+
+    glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _textureId);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glScalef(0.15,0.15,0.15);
+    glBegin(GL_QUADS);
+	glNormal3f(0.0f, 1.0f, 0.0f);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, 2.0f);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, 2.0f);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, 2.0f);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, 2.0f);
+	glEnd();
+
+	glDisable(GL_TEXTURE_2D);
+}
+
 void levelOne(){
 
     float tx=-1.2;
@@ -136,6 +190,11 @@ void levelOne(){
         glutSolidCube(0.4);
         glColor3f(0.7,0.7,0.7);
         glutWireCube(0.4);
+
+        glPushMatrix();
+        drawQuad();
+        glPopMatrix();
+
         glPopMatrix();
 
         tx+=0.6;
@@ -708,8 +767,9 @@ int main(int argc, char *argv[])
     glutInitWindowSize(640,480);
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("Avance");
+    glutCreateWindow("Project/LYNX");
     init();
+    initRendering();
     glutDisplayFunc(display);
     glutKeyboardFunc(myKeyboard);
     glutTimerFunc(1,myTimer,1);
@@ -718,4 +778,182 @@ int main(int argc, char *argv[])
     glutSpecialFunc(specialKeys);
     glutMainLoop();
     return EXIT_SUCCESS;
+}
+
+#include <assert.h>
+#include <fstream>
+
+#include "imageloader.h"
+
+using namespace std;
+
+Image::Image(char* ps, int w, int h) : pixels(ps), width(w), height(h) {
+
+}
+
+Image::~Image() {
+	delete[] pixels;
+}
+
+namespace {
+	//Converts a four-character array to an integer, using little-endian form
+	int toInt(const char* bytes) {
+		return (int)(((unsigned char)bytes[3] << 24) |
+					 ((unsigned char)bytes[2] << 16) |
+					 ((unsigned char)bytes[1] << 8) |
+					 (unsigned char)bytes[0]);
+	}
+
+	//Converts a two-character array to a short, using little-endian form
+	short toShort(const char* bytes) {
+		return (short)(((unsigned char)bytes[1] << 8) |
+					   (unsigned char)bytes[0]);
+	}
+
+	//Reads the next four bytes as an integer, using little-endian form
+	int readInt(ifstream &input) {
+		char buffer[4];
+		input.read(buffer, 4);
+		return toInt(buffer);
+	}
+
+	//Reads the next two bytes as a short, using little-endian form
+	short readShort(ifstream &input) {
+		char buffer[2];
+		input.read(buffer, 2);
+		return toShort(buffer);
+	}
+
+	//Just like auto_ptr, but for arrays
+	template<class T>
+	class auto_array {
+		private:
+			T* array;
+			mutable bool isReleased;
+		public:
+			explicit auto_array(T* array_ = NULL) :
+				array(array_), isReleased(false) {
+			}
+
+			auto_array(const auto_array<T> &aarray) {
+				array = aarray.array;
+				isReleased = aarray.isReleased;
+				aarray.isReleased = true;
+			}
+
+			~auto_array() {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+			}
+
+			T* get() const {
+				return array;
+			}
+
+			T &operator*() const {
+				return *array;
+			}
+
+			void operator=(const auto_array<T> &aarray) {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+				array = aarray.array;
+				isReleased = aarray.isReleased;
+				aarray.isReleased = true;
+			}
+
+			T* operator->() const {
+				return array;
+			}
+
+			T* release() {
+				isReleased = true;
+				return array;
+			}
+
+			void reset(T* array_ = NULL) {
+				if (!isReleased && array != NULL) {
+					delete[] array;
+				}
+				array = array_;
+			}
+
+			T* operator+(int i) {
+				return array + i;
+			}
+
+			T &operator[](int i) {
+				return array[i];
+			}
+	};
+}
+
+Image* loadBMP(const char* filename) {
+	ifstream input;
+	input.open(filename, ifstream::binary);
+	assert(!input.fail() || !"Could not find file");
+	char buffer[2];
+	input.read(buffer, 2);
+	assert(buffer[0] == 'B' && buffer[1] == 'M' || !"Not a bitmap file");
+	input.ignore(8);
+	int dataOffset = readInt(input);
+
+	//Read the header
+	int headerSize = readInt(input);
+	int width;
+	int height;
+	switch(headerSize) {
+		case 40:
+			//V3
+			width = readInt(input);
+			height = readInt(input);
+			input.ignore(2);
+			assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+			assert(readShort(input) == 0 || !"Image is compressed");
+			break;
+		case 12:
+			//OS/2 V1
+			width = readShort(input);
+			height = readShort(input);
+			input.ignore(2);
+			assert(readShort(input) == 24 || !"Image is not 24 bits per pixel");
+			break;
+		case 64:
+			//OS/2 V2
+			assert(!"Can't load OS/2 V2 bitmaps");
+			break;
+		case 108:
+			//Windows V4
+			assert(!"Can't load Windows V4 bitmaps");
+			break;
+		case 124:
+			//Windows V5
+			assert(!"Can't load Windows V5 bitmaps");
+			break;
+		default:
+			assert(!"Unknown bitmap format");
+	}
+
+	//Read the data
+	int bytesPerRow = ((width * 3 + 3) / 4) * 4 - (width * 3 % 4);
+	int size = bytesPerRow * height;
+	auto_array<char> pixels(new char[size]);
+	input.seekg(dataOffset, ios_base::beg);
+	input.read(pixels.get(), size);
+
+	//Get the data into the right format
+	auto_array<char> pixels2(new char[width * height * 3]);
+	for(int y = 0; y < height; y++) {
+		for(int x = 0; x < width; x++) {
+			for(int c = 0; c < 3; c++) {
+				pixels2[3 * (width * y + x) + c] =
+					pixels[bytesPerRow * y + 3 * x + (2 - c)];
+			}
+		}
+	}
+
+	input.close();
+	return new Image(pixels2.release(), width, height);
 }
